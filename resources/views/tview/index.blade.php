12cb4327d864e54f8cc5e228c1bfdb19bba9f0d8
jednalig <99207298+jednalig@users.noreply.github.com>
2022-12-08 14:19:11 +0800

    Update index.blade.php

--- resources/views/tview/index.blade.php
+++ resources/views/tview/index.blade.php
@@ -1,93 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    <center><h3 class="page-title">Trivia's and Facts</h3></center>
+
+
 <head>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
-   
-    <div class="col-lg-12">
-		<div class="card">
-			<div class="card-header">
-			
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            @lang('quickadmin.list')
+        </div>
+
+        <div class="panel-body">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <table class="table table-bordered table-striped {{ count($uploads) > 0 ? 'datatable' : '' }}">
+                <thead>
+                    <tr>
+                        {{-- <th style="text-align:center;"><input type="checkbox" id="select-all" /></th> --}}
+                        <th><center><h3><bold>Trivia/Facts</bold></h3></center></th>
+                        {{-- <th>&nbsp;</th> --}}
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    @if (count($uploads) > 0)
+                        @foreach ($uploads as $upload)
+                            <tr>
+                                {{-- <td></td> --}}
+                                <td style="padding: 50px"><h4>{{ $upload->description }}</h4></td>
+                              
+                                    {{-- <a href="{{ route('uploads.show',[$upload->id]) }}" class="btn btn-xs btn-primary">@lang('quickadmin.view')</a> --}}
+                                    {{-- <a href="{{ route('uploads.edit',[$upload->id]) }}" class="btn btn-xs btn-info">@lang('quickadmin.edit')</a> --}}
+                                    {{-- {!! Form::open(array( --}}
+                                   {{-- /     'style' => 'display: inline-block;', --}}
+                                        {{-- 'method' => 'DELETE', --}}
+                                       {{-- / 'onsubmit' => "return confirm('".trans("quickadmin.are_you_sure")."');", --}}
+                                        {{-- 'route' => ['uploads.destroy', $upload->id])) !!} --}}
+                                    {{-- {!! Form::submit(trans('quickadmin.delete'), array('class' => 'btn btn-xs btn-danger')) !!} --}}
+                                    {{-- {!! Form::close() !!} --}}
+                              
+                            </tr>
+                        @endforeach
+                    @else
+                        <tr>
+                            {{-- <td colspan="3"></td> --}}
+                        </tr>
+                    @endif
+                </tbody>
+            </table>
 			</div>
-			<div class="card-body">
-				<table class="table table-bordered table-hover" id="complaint-tbl">
-			        <thead>
-			          <tr>
-			           
-			          
-						<th></th>
-                       
-					
-			        
-			           
-			          </tr>
-			        </thead>
-			        <tbody>
-			          <?php
-			          $conn= new mysqli('localhost','root','','jftq')or die("Could not connect to mysql".mysqli_error($con));
-			          $qry = $conn->query("SELECT * FROM uploads");
-			          while($row = $qry->fetch_array()):
-			          ?>
-			       
-					
-						<div id="myCarousel" class="carousel slide" data-ride="carousel">
-						  <!-- Indicators -->
-						  <ol class="carousel-indicators">
-							<li data-target="#myCarousel" data-slide-to=" <?php echo $row['id'] ?>" ></li>
-							
-						  </ol>
-					  
-						  <!-- Wrapper for slides -->
-						  <div class="carousel-inner">
-					  
-							<div class="item">
-								<h3> <?php echo $row['description'] ?></h3>
-							  {{-- <img src="la.jpg" alt="Los Angeles" style="width:100%;"> --}}
-							  <div class="carousel-caption">
-								<h3> <?php echo $row['description'] ?></h3>
-								<p><?php echo $row['description'] ?></p>
-							  </div>
-							</div>
-					  
-							
-						
-						  </div>
-					  
-						
-						
-						 <td style="padding:100px"><center><h3><?php echo $row['description'] ?><h3></center></td>
-			         
-						
-                     
-			          
-			          </tr>
-			        <?php endwhile; ?>
-					  <!-- Left and right controls -->
-					  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-						<span class="glyphicon glyphicon-chevron"></span>
-						<span class="sr-only"></span>
-					  </a>
-					  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-						<span class="glyphicon glyphicon-chevron"></span>
-						<span class="sr-only"></span>
-					  </a>
-					</div>
-			
-			  
-			        </tbody>
-			  </table>
-			</div>
-		</div>
-	</div>
-  
+        </div>
+    </div>
 @stop
 
 @section('javascript')
     <script>
-        window.route_mass_crud_entries_destroy = '{{ route('topics.mass_destroy') }}';
+        // window.route_mass_crud_entries_destroy = '{{ route('uploads.mass_destroy') }}';
     </script>
 @endsection
